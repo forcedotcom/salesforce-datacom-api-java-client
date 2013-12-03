a13f8dfcf33e41bc3802399ee17b9494247ed43b
akolesnik <akolesnik@AKolesnik-WS.internal.salesforce.com>
2013-12-03 14:26:17 -0800

    test

---
+++ src/main/java/com/salesforce/datacom/api/client/Test.java
@@ -0,0 +1,16 @@
+package com.salesforce.datacom.api.client;
+
+
+/**
+ * @author Aleksey Kolesnik
+ */
+public class Test {
+    
+    
+    public static void main(String... str) {
+//        ContactsService myService = new ContactsService("<var>YOUR_APPLICATION_NAME</var>");
+
+        System.out.println("Hello World!");
+    }
+    
+}
